e1ab63a36b834bbba6d2482f85b718db30a1b946
Suneeth Varghese <suneethvarghese@Suneeths-MacBook-Pro.local>
2018-02-18 14:04:30 -0500

    ArrayRotation class with tests.

---
+++ HackerRank/ArrayRotation.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace HackerRank
+{
+    public class ArrayRotation
+    {   
+        private string  _array="";
+        public ArrayRotation(string array)
+        {
+            _array=array;
+        }
+
+        public int[] RotateRight(int arraylength,int rotations)
+        {
+           
+
+                int n = arraylength;
+
+                int k = rotations;
+
+                string[] a_temp = _array.Split(' ');
+
+                int[] a = Array.ConvertAll(a_temp,Int32.Parse);
+                 
+
+                int[] ta=new int[n];
+                if(k==0){
+                    return a;
+                }
+
+                for(int i=0; i< n;i++)
+                {
+                    if(i >= n-k) // 3
+                        {
+                            ta[i-(n-k)]= a[i]; 
+                        }
+                    else
+                        {
+                            ta[i+k]= a[i]; 
+                        }
+                }
+                return ta;
+        }
+
+        public int[] RotateLeft(int arraylength, int rotations)
+        {
+               int n = arraylength;
+
+                int k = rotations;
+
+                string[] a_temp = _array.Split(' ');
+
+                int[] a = Array.ConvertAll(a_temp,Int32.Parse);
+                 
+
+                int[] ta=new int[n];
+                if(k==0){
+                    return a;
+                }
+
+                for(int i=n-1; i >= 0; i--)
+                {
+                    if(i >=n-k)  //>= 4
+                    {
+                       ta[i]=a[i-(n-k)];
+                    }
+                    else
+                    {
+                       ta[i]= a[i+k];     
+                    }
+                }
+
+                return ta;
+        }
+    }
+     
+}
---
+++ HackerRankTests/ArrayRotationTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using HackerRank;
+namespace HackerRankTests
+{
+    [TestFixture]
+    public class ArrayRotationTests
+    {
+        private ArrayRotation _sut;
+
+        [SetUp]
+        public void Init()
+        {
+          _sut=new ArrayRotation("1 2 3 4 5");
+        }
+        [Test]
+        public void Should_return_array_zero_times_rotated()
+        {
+            var result= _sut.RotateRight(5,0);
+           var expected = new int[]{1 ,2, 3, 4 ,5};
+             Assert.AreEqual(result,expected);
+        }
+
+        //[TestCase(5,1)]
+        [TestCase(5,3)]
+
+        public void Should_return_array_r_time_rotated(int length,int r)
+        {
+          var result=_sut.RotateRight(length,r);
+          var expected=new int[]{3,4,5,1,2};
+          Assert.AreEqual(expected,result);
+        }
+
+        [TestCase(5,1, new int[]{2 ,3 ,4 ,5 ,1})]
+        [TestCase(5,2, new int[]{3 ,4 ,5 ,1 ,2})]
+        [TestCase(5,3, new int[]{4 ,5 ,1 ,2 ,3})]
+        [TestCase(5,4, new int[]{5 ,1 ,2 ,3 ,4})]
+        public void Should_return_array_r_times_left_rotated(int length,int r,int[] expected)
+        {
+           var result=_sut.RotateLeft(length,r);
+            Assert.AreEqual(expected,result);
+        }
+
+    }
+}
